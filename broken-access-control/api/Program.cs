0445d8ea7a79179de5187e6a97915f12c2a1b534
Bjørn Vinther <bjorn.vinther@lego.com>
2025-05-23 10:11:57 +0200

    can run via docker

--- broken-access-control/api/Program.cs
+++ broken-access-control/api/Program.cs
@@ -44,11 +44,8 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
--- broken-access-control/client/src/api.js
+++ broken-access-control/client/src/api.js
@@ -1,35 +1,34 @@
+import axios from 'axios';
 
-// api.js
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
-const API_BASE = 'https://localhost:32770/api';
-
-async function loginUser(username, password) {
+export async function loginUser(username, password) {
     const response = await axios.post(`${API_BASE}/login`, { username, password });
     axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
     return response.status === 200;
 }
 
-async function getPosts() {
+export async function getPosts() {
     const response = await axios.get(`${API_BASE}/posts`);
     return response.data;
 }
 
-async function getPost(postId) {
+export async function getPost(postId) {
     const response = await axios.get(`${API_BASE}/posts/${postId}`);
     return response.data;
 }
 
-async function createPost(title, content, owner) {
+export async function createPost(title, content, owner) {
     const response = await axios.post(`${API_BASE}/posts`, { title, content, owner });
     return response.status === 200 || response.status === 201;
 }
 
-async function postComment(postId, text, owner) {
+export async function postComment(postId, text, owner) {
     const response = await axios.post(`${API_BASE}/posts/${postId}/comments`, { text, owner });
     return response.status === 200 || response.status === 201;
 }
 
-async function deleteComment(postId, commentId) {
+export async function deleteComment(postId, commentId) {
     const response = await axios.delete(`${API_BASE}/posts/${postId}/comments/${commentId}`);
     return response.status === 200 || response.status === 204;
 }
